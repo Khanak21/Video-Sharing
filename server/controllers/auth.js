ca55335f1a153e1de7bf55d78ca04df74bf64c27
Khanak21 <khanakpatwari21@gmail.com>
2023-08-27 04:07:49 +0530

    second commit

---
+++ server/controllers/auth.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import User from  "../models/user.js"
+import bcrypt from "bcryptjs";
+import  jwt  from "jsonwebtoken";
+
+export const ErrorMessage = (status,message)=>{
+  const error = new Error();
+  error.status = status;
+  error.message = message;
+  return error;
+}
+
+export const signup = async(req,res,next)=>{
+    try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({ ...req.body, password: hash });
+
+    await newUser.save();
+    res.status(200).send("Aapka Swagat Hai");
+  } catch (err) {
+    next(err)
+  }
+};
+
+
+
+export const signin = async (req, res, next) => {
+
+  try {
+    
+    const user = await User.findOne({ name: req.body.name });
+    if (!user) return next(ErrorMessage(404, "User not found!"));
+
+    const isCorrect = await bcrypt.compare(req.body.password, user.password);
+
+    if (!isCorrect) return next(ErrorMessage(400, "Wrong Credentials!"));
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT);
+    const { password, ...others } = user._doc;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ server/routes/Video.js
@@ -0,0 +1,34 @@
+import express from "express";
+import {AddVideo,EditVideo,GetVideo,DeleteVideo,addView,trend,random,subscribedVideos,tagVideo,search, likeVideo, dislikeVideo} from '../controllers/Videos.js'
+import { verifyToken } from "../Verify.js";
+const router = express.Router();
+
+export const authMiddleware = (req, res, next) => {
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return res.status(401).json({ message: "Authorization token missing" });
+    }
+  
+    try {
+      const decoded = jwt.verify(token, process.env.JWT);
+      req.user = decoded;
+      next();
+    } catch (err) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+};
+  
+
+router.post("/",AddVideo);
+router.put("/:id",authMiddleware,EditVideo)
+router.get("/GetVideo/:id",authMiddleware,GetVideo)
+router.delete("/delete/:id",authMiddleware,DeleteVideo)
+router.put("/addView/:id", authMiddleware,addView)
+router.get("/mostviewed",trend)
+router.get("/random",random)
+router.get("/sub",verifyToken,subscribedVideos)
+router.get("/tags",tagVideo)
+router.get("/search",search)
+router.put("/like/:id",likeVideo)
+router.put("/dislike/:id",authMiddleware,dislikeVideo)
+export default router;
